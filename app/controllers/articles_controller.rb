086780ec629dde4233a1433659854b0a8ee5c6c3
BiancaLeitner <bianca.leitner@beecoding.at>
2017-04-14 18:05:50 +0200

    :sparkles: Feat: implemented saving, show and list functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,30 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    # find the article we're interested in - pass in params[.id] to get
+    # :id parameter from the request and save it in instance of article object.
+    @article = Article.find(params[:id])
+  end
 
   def new
   end
 
   def create
-    # The render method here is taking a very simple hash
-    # with a key of :plain and value of params[:article].inspect.
-    #
-    # The params method is the object which represents the parameters 
-    # (or fields) coming in from the form. 
-    # --> returns an ActionController::Parameters object, which allows 
-    #     you to access the keys of the hash using either strings or symbols. 
-    render plain: params[:article].inspect
+    # initialize model with  attributes, map them to db columns and
+    @article = Article.new(article_params)
+    # save model in db
+    @article.save
+    # redirect to show action
+    redirect_to @article
   end
+
+  private
+    def article_params
+      # whitelist controller parameters to prevent wrongful mass assignment
+      params.require(:article).permit(:title, :text)
+    end
 
 end
