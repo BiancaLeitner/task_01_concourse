5104ecb3078fffc73a0230ee8f8728c20143d68e
BiancaLeitner <bianca.leitner@beecoding.at>
2017-04-18 02:15:49 +0200

    :white_check_mark: Test: pushed wrong test-data

--- spec/articles/creating_article_spec.rb
+++ spec/articles/creating_article_spec.rb
@@ -7,7 +7,7 @@ feature 'Creating article' do
     # 2. Click on the "New article" button
     click_link 'New article'
     # 3. Fill out the form - add a title  with at least 5 characters and a text with at least 100 characters
-    fill_in 'Title', :with => 'Lorem'
+    fill_in 'Title', :with => 'Lo'
     fill_in 'Text', :with => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut l'
     # 4. Submit the form
     click_button 'Create Article'
