1316eda8884e1ece8fd0edd7fcc1493884f51646
BiancaLeitner <bianca.leitner@beecoding.at>
2017-04-18 08:30:02 +0200

    :white_check_mark: Test: fixed test (pipeline should not throw error)

--- spec/articles/creating_article_spec.rb
+++ spec/articles/creating_article_spec.rb
@@ -7,10 +7,10 @@ feature 'Creating article' do
     # 2. Click on the "New article" button
     click_link 'New article'
     # 3. Fill out the form - add a title  with at least 5 characters and a text with at least 100 characters
-    fill_in 'Title', :with => ''
+    fill_in 'Title', :with => 'Test'
     # 4. Submit the form
     click_button 'Create Article'
     # 5. See the 'show' page of created article
-    expect(page).to have_css("p", :text => 'Bianca')
+    expect(page).to have_css("p", :text => 'Test')
   end
 end
