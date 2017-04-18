9a50cfffef538f40d24a263557796bec158a050f
BiancaLeitner <bianca.leitner@beecoding.at>
2017-04-18 02:58:00 +0200

    :bug: Fix: fixed bug in test

--- spec/articles/creating_article_spec.rb
+++ spec/articles/creating_article_spec.rb
@@ -7,11 +7,10 @@ feature 'Creating article' do
     # 2. Click on the "New article" button
     click_link 'New article'
     # 3. Fill out the form - add a title  with at least 5 characters and a text with at least 100 characters
-    fill_in 'Title', :with => 'Lo'
-    fill_in 'Text', :with => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut l'
+    fill_in 'Title', :with => ''
     # 4. Submit the form
     click_button 'Create Article'
     # 5. See the 'show' page of created article
-    expect(page).to have_content('Lorem ipsum dolor sit amet')
+    expect(page).to have_css("p", :text => 'Bianca')
   end
 end
