fa88cf5aae75a6b74b0691594276b14ff358a156
BiancaLeitner <bianca.leitner@beecoding.at>
2017-04-19 17:00:14 +0200

    :bug: Fix: brakemen_output versions bug

--- ci/test.sh
+++ ci/test.sh
@@ -13,11 +13,15 @@ pushd task_01_concourse
   git checkout brakeman
   # create new brakeman file
   brakeman -o brakeman_output.json
-  git config --global user.email "bianca.leitner@beecoding.at"
-  git config --global user.name "BiancaLeitner"
-  # commit and push it to the branch
-  git commit -m "new version of brakeman-file"
-  git push -u origin brakeman
+  if [ -n "$(git status --porcelain)" ]; then
+    git config --global user.email "bianca.leitner@beecoding.at"
+    git config --global user.name "BiancaLeitner" 
+    # commit and push it to the branch
+    git commit -m "new version of brakeman-file"
+    git push -u origin brakeman
+  else 
+    echo "files are identical";
+  fi
   # return to master
   git checkout master
 popd
