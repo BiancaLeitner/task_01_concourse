6479c183cf3cc27f34ea0d65dabf7c138cdfef7b
BiancaLeitner <bianca.leitner@beecoding.at>
2017-04-19 16:01:13 +0200

    :white_check_mark: Test: test if brakeman version compare works

--- ci/test.sh
+++ ci/test.sh
@@ -5,10 +5,17 @@ set -e -x
 pushd task_01_concourse
   bundle install
   bundle exec rspec
-  bundle exec brakeman -o brakeman_output.json
-  git checkout -b brakeman
-  git push -u origin brakeman
-  git checkout master
+  # copy old brakeman file from branch
   git checkout brakeman -- brakeman_output.json
+  # compare
   brakeman --compare brakeman_output.json
+  # switch to branch
+  git checkout brakeman
+  # create new brakeman file
+  brakeman -o brakeman_output.json
+  # commit and push it to the branch
+  git commit -m "new version of brakeman-file"
+  git push
+  # return to master
+  git checkout master
 popd
