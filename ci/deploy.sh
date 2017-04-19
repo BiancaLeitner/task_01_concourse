1bf5964a0cc102f31a133a76ac71ecd99bf64ef7
BiancaLeitner <bianca.leitner@beecoding.at>
2017-04-19 19:50:39 +0200

    :hammer: Refactor: splitted pipeline in different yml files and shell scripts

---
+++ ci/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e -x
+
+pushd git-repository
+  cat > ~/.netrc <<EOF
+  machine git.heroku.com
+    login $HEROKU_EMAIL
+    password $HEROKU_TOKEN
+  EOF
+  git push https://git.heroku.com/task-01-concourse.git master:refs/heads/master
+popd
--- ci/test.sh
+++ ci/test.sh
@@ -2,25 +2,8 @@
 
 set -e -x
 
-pushd task_01_concourse
+pushd git-repository
   bundle install
-  bundle exec rspec
-  # copy old brakeman file from branch
-  git checkout brakeman -- brakeman_output.json
-  # compare
-  brakeman --compare brakeman_output.json
-  # switch to branch
-  git checkout brakeman
-  # create new brakeman file
-  brakeman -o brakeman_output.json
-  if [ -n "$(git status --porcelain)" ]; then
-    git add brakeman_output.json
-    # commit and push it to the branch
-    git commit -m "new version of brakeman-file"
-    git push -u origin brakeman
-  else 
-    echo "files are identical";
-  fi
-  # return to master
-  git checkout master
+  rspec
+  brakeman
 popd
